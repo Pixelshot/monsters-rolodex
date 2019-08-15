af8914505db09a14fc535f67894b1d03bb24644c
Hazri <hazrijordan@gmail.com>
2019-08-15 16:09:33 +0800

    Updates

---
+++ precache-manifest.48ed5ccf42e9885a466da2d9695925bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b28a68484ddd07242e1bcd169685db",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cdcc4feb58fc73922780",
+    "url": "/monsters-rolodex/static/css/main.51b2d61d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "cdcc4feb58fc73922780",
+    "url": "/monsters-rolodex/static/js/main.b11de7a4.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
